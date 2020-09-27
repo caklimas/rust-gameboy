4a0f5018b43807543b274965ba8219696538aba9
Christopher Klimas <caklimas@gmail.com>
2020-09-27 15:23:09 -0400

    Start implementing interrupts

---
+++ src/mmu/interrupts/mod.rs
@@ -0,0 +1,14 @@
+use serde::{Serialize, Deserialize};
+
+bitfield!{
+    #[derive(Serialize, Deserialize, Default)]
+    pub struct Interrupt(u8);
+    impl Debug;
+
+    pub v_blank, set_v_blank: 0;
+    pub lcd_stat, set_lcd_stat: 1;
+    pub timer, set_timer: 2;
+    pub serial, set_serial: 3;
+    pub joypad, set_joypad: 4;
+    pub get, set: 4, 0;
+}
