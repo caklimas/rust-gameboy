fae9861f156dd9c136fd6f747f1b6defc9294ad6
caklimas <caklimas@gmail.com>
2020-12-28 21:03:16 -0500

    Add rendering for testing

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,28 +1,28 @@
 extern crate caklimas_rust_gameboy as rust_gameboy;
 
-// use ggez::ContextBuilder;
-// use ggez::conf::{WindowMode, WindowSetup};
-// use ggez::event;
-// use std::fs;
+use ggez::ContextBuilder;
+use ggez::conf::{WindowMode, WindowSetup};
+use ggez::event;
+use std::fs;
 
-// use rust_gameboy::constants::screen::*;
+use rust_gameboy::constants::screen::*;
 
 fn main() {
-    // let directory = std::env::current_dir().expect("Can't get current directory");
-    // let path = directory.join(r"H:\Repos\rust-gameboy\src\gameboy\tests\roms\02-interrupts.gb");
-    // let bytes = fs::read(path).expect("Error reading file");
-    // let cb = get_context_builder();
-    // let (ctx, event_loop) = &mut cb.build().expect("Error creating event loop");
-    // let mut gameboy = rust_gameboy::gameboy::Gameboy::new(bytes, true);
-    // event::run(ctx, event_loop, &mut gameboy).expect("Error running event loop");
+    let directory = std::env::current_dir().expect("Can't get current directory");
+    let path = directory.join(r"C:\Users\Christopher\Downloads\Super Mario Land.gb");
+    let bytes = fs::read(path).expect("Error reading file");
+    let cb = get_context_builder();
+    let (ctx, event_loop) = &mut cb.build().expect("Error creating event loop");
+    let mut gameboy = rust_gameboy::gameboy::Gameboy::new(bytes, true);
+    event::run(ctx, event_loop, &mut gameboy).expect("Error running event loop");
 }
 
-// fn get_context_builder() -> ggez::ContextBuilder {
-//     let window_setup: WindowSetup = Default::default();
-//     let window_mode: WindowMode = Default::default();
-//     let width = (SCREEN_WIDTH as usize) * PIXEL_SIZE;
-//     let height = (SCREEN_HEIGHT as usize) * PIXEL_SIZE;
-//     ggez::ContextBuilder::new("Rust Gameboy", "caklimas")
-//         .window_setup(window_setup.title("Gameboy"))
-//         .window_mode(window_mode.dimensions(width as f32, height as f32))
-// }
+fn get_context_builder() -> ggez::ContextBuilder {
+    let window_setup: WindowSetup = Default::default();
+    let window_mode: WindowMode = Default::default();
+    let width = (SCREEN_WIDTH as usize) * PIXEL_SIZE;
+    let height = (SCREEN_HEIGHT as usize) * PIXEL_SIZE;
+    ggez::ContextBuilder::new("Rust Gameboy", "caklimas")
+        .window_setup(window_setup.title("Gameboy"))
+        .window_mode(window_mode.dimensions(width as f32, height as f32))
+}
--- src/gameboy/render.rs
+++ src/gameboy/render.rs
@@ -1,54 +1,59 @@
-// use ggez::{Context, GameResult};
-// use ggez::event;
-// use ggez::graphics::{self, Color, DrawMode, DrawParam, FillOptions, MeshBuilder};
+use ggez::{Context, GameResult};
+use ggez::event;
+use ggez::graphics::{self, Color, DrawMode, DrawParam, FillOptions, MeshBuilder};
 
-// use super::Gameboy;
-// use crate::constants::gpu::*;
-// use crate::constants::screen::*;
+use super::Gameboy;
+use crate::constants::gpu::*;
+use crate::constants::screen::*;
 
-// impl Gameboy {
-//     fn draw_screen(&mut self, ctx: &mut Context) {
-//         let mut builder = MeshBuilder::new();
-//         let screen = self.get_screen();
-//         let mut coordinates = 0usize;
-//         for i in (0..screen.len()).step_by(COLOR_PER_PIXEL) {
-//             let r = screen[i];
-//             let g = screen[i + 1];
-//             let b = screen[i + 2];
-//             let color = Color::from_rgb(r, g, b);
-//             let y_offset = (coordinates / (SCREEN_WIDTH as usize)) * PIXEL_SIZE;
-//             let x_offset = (coordinates % (SCREEN_WIDTH as usize)) * PIXEL_SIZE;
-//             let rectangle = graphics::Rect::new(
-//                 x_offset as f32,
-//                 y_offset as f32,
-//                 PIXEL_SIZE as f32,
-//                 PIXEL_SIZE as f32
-//             );
-//             builder.rectangle(DrawMode::Fill(FillOptions::DEFAULT), rectangle, color);
-//             coordinates += 1;
-//         }
+impl Gameboy {
+    fn draw_screen(&mut self, ctx: &mut Context) {
+        let mut builder = MeshBuilder::new();
+        let screen = self.get_screen();
+        let mut coordinates = 0usize;
+        for i in (0..screen.len()).step_by(COLOR_PER_PIXEL) {
+            let r = screen[i];
+            let g = screen[i + 1];
+            let b = screen[i + 2];
+            let color = Color::from_rgb(r, g, b);
+            let y_offset = (coordinates / (SCREEN_WIDTH as usize)) * PIXEL_SIZE;
+            let x_offset = (coordinates % (SCREEN_WIDTH as usize)) * PIXEL_SIZE;
+            let rectangle = graphics::Rect::new(
+                x_offset as f32,
+                y_offset as f32,
+                PIXEL_SIZE as f32,
+                PIXEL_SIZE as f32
+            );
 
-//         let mesh = builder.build(ctx).expect("Error building screen");
-//         graphics::draw(ctx, &mesh, DrawParam::new()).expect("Error drawing the screen");
-//     }
-// }
+            if color != Color::from_rgb(RGB_WHITE.0, RGB_WHITE.1, RGB_WHITE.2) {
+                let aasd = 213;
+            }
 
-// impl event::EventHandler for Gameboy {
-//     fn update(&mut self, _ctx: &mut Context) -> GameResult {
-//         Ok(())
-//     }
+            builder.rectangle(DrawMode::Fill(FillOptions::DEFAULT), rectangle, color);
+            coordinates += 1;
+        }
 
-//     fn draw(&mut self, ctx: &mut Context) -> GameResult {
-//         graphics::clear(ctx, graphics::WHITE);
-//         let mut frame_complete = false;
-//         while !frame_complete {
-//             self.clock();
-//             frame_complete = self.frame_complete();
-//         }
+        let mesh = builder.build(ctx).expect("Error building screen");
+        graphics::draw(ctx, &mesh, DrawParam::new()).expect("Error drawing the screen");
+    }
+}
 
-//         self.draw_screen(ctx);
-//         graphics::present(ctx).expect("Error rendering the screen");
+impl event::EventHandler for Gameboy {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult {
+        Ok(())
+    }
 
-//         Ok(())
-//     }
-// }
+    fn draw(&mut self, ctx: &mut Context) -> GameResult {
+        graphics::clear(ctx, graphics::WHITE);
+        let mut frame_complete = false;
+        while !frame_complete {
+            self.clock();
+            frame_complete = self.frame_complete();
+        }
+
+        self.draw_screen(ctx);
+        graphics::present(ctx).expect("Error rendering the screen");
+
+        Ok(())
+    }
+}
