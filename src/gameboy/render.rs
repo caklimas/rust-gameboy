f067ba26e2e8b6395b9fd1e3c9dd5540d99f6496
caklimas <caklimas@gmail.com>
2021-04-21 19:03:31 -0400

    Commented out ggez

--- src/gameboy/render.rs
+++ src/gameboy/render.rs
@@ -1,55 +1,54 @@
-use ggez::{Context, GameResult};
-use ggez::event;
-use ggez::graphics::{self, Color, DrawMode, DrawParam, FillOptions, MeshBuilder};
+// use ggez::{Context, GameResult};
+// use ggez::event;
+// use ggez::graphics::{self, Color, DrawMode, DrawParam, FillOptions, MeshBuilder};
 
-use super::Gameboy;
-use crate::constants::gpu::*;
-use crate::constants::screen::*;
+// use super::Gameboy;
+// use crate::constants::gpu::*;
+// use crate::constants::screen::*;
 
-impl Gameboy {
-    fn draw_screen(&mut self, ctx: &mut Context) {
-        let mut builder = MeshBuilder::new();
-        let screen = self.get_screen();
-        let mut coordinates = 0usize;
-        for i in (0..screen.len()).step_by(COLOR_PER_PIXEL) {
-            let r = screen[i];
-            let g = screen[i + 1];
-            let b = screen[i + 2];
-            let color = Color::from_rgb(r, g, b);
-            let y_offset = (coordinates / (SCREEN_WIDTH as usize)) * PIXEL_SIZE;
-            let x_offset = (coordinates % (SCREEN_WIDTH as usize)) * PIXEL_SIZE;
-            let rectangle = graphics::Rect::new(
-                x_offset as f32,
-                y_offset as f32,
-                PIXEL_SIZE as f32,
-                PIXEL_SIZE as f32
-            );
+// impl Gameboy {
+//     fn draw_screen(&mut self, ctx: &mut Context) {
+//         let mut builder = MeshBuilder::new();
+//         let screen = self.get_screen();
+//         let mut coordinates = 0usize;
+//         for i in (0..screen.len()).step_by(COLOR_PER_PIXEL) {
+//             let r = screen[i];
+//             let g = screen[i + 1];
+//             let b = screen[i + 2];
+//             let color = Color::from_rgb(r, g, b);
+//             let y_offset = (coordinates / (SCREEN_WIDTH as usize)) * PIXEL_SIZE;
+//             let x_offset = (coordinates % (SCREEN_WIDTH as usize)) * PIXEL_SIZE;
+//             let rectangle = graphics::Rect::new(
+//                 x_offset as f32,
+//                 y_offset as f32,
+//                 PIXEL_SIZE as f32,
+//                 PIXEL_SIZE as f32
+//             );
+//             builder.rectangle(DrawMode::Fill(FillOptions::DEFAULT), rectangle, color);
+//             coordinates += 1;
+//         }
 
-            builder.rectangle(DrawMode::Fill(FillOptions::DEFAULT), rectangle, color);
-            coordinates += 1;
-        }
+//         let mesh = builder.build(ctx).expect("Error building screen");
+//         graphics::draw(ctx, &mesh, DrawParam::new()).expect("Error drawing the screen");
+//     }
+// }
 
-        let mesh = builder.build(ctx).expect("Error building screen");
-        graphics::draw(ctx, &mesh, DrawParam::new()).expect("Error drawing the screen");
-    }
-}
+// impl event::EventHandler for Gameboy {
+//     fn update(&mut self, _ctx: &mut Context) -> GameResult {
+//         Ok(())
+//     }
 
-impl event::EventHandler for Gameboy {
-    fn update(&mut self, _ctx: &mut Context) -> GameResult {
-        Ok(())
-    }
+//     fn draw(&mut self, ctx: &mut Context) -> GameResult {
+//         graphics::clear(ctx, graphics::WHITE);
+//         let mut frame_complete = false;
+//         while !frame_complete {
+//             self.clock();
+//             frame_complete = self.frame_complete();
+//         }
 
-    fn draw(&mut self, ctx: &mut Context) -> GameResult {
-        graphics::clear(ctx, graphics::WHITE);
-        let mut frame_complete = false;
-        while !frame_complete {
-            self.clock();
-            frame_complete = self.frame_complete();
-        }
+//         self.draw_screen(ctx);
+//         graphics::present(ctx).expect("Error rendering the screen");
 
-        self.draw_screen(ctx);
-        graphics::present(ctx).expect("Error rendering the screen");
-
-        Ok(())
-    }
-}
+//         Ok(())
+//     }
+// }
