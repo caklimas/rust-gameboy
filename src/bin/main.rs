2abaa18ec10ef694df4df1dd4f4a96d5b0e554ee
Christopher Klimas <caklimas@gmail.com>
2021-07-17 22:55:45 -0400

    Comment out main

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,4 +1,4 @@
-// extern crate caklimas_rust_gameboy as rust_gameboy;
+extern crate caklimas_rust_gameboy as rust_gameboy;
 
 // use ggez::ContextBuilder;
 // use ggez::conf::{WindowMode, WindowSetup};
@@ -8,13 +8,13 @@
 // use rust_gameboy::constants::screen::*;
 
 fn main() {
-//     let directory = std::env::current_dir().expect("Can't get current directory");
-//     let path = directory.join(r"C:\Users\cakli\source\repos\rust-gameboy-web\public\roms\Kirby's Dream Land.gb");
-//     let bytes = fs::read(path).expect("Error reading file");
-//     let cb = get_context_builder();
-//     let (ctx, event_loop) = &mut cb.build().expect("Error creating event loop");
-//     let mut gameboy = rust_gameboy::gameboy::Gameboy::new(bytes, true);
-//     event::run(ctx, event_loop, &mut gameboy).expect("Error running event loop");
+    // let directory = std::env::current_dir().expect("Can't get current directory");
+    // let path = directory.join(r"H:\Repos\rust-gameboy-web\public\roms\Dr. Mario.gb");
+    // let bytes = fs::read(path).expect("Error reading file");
+    // let cb = get_context_builder();
+    // let (ctx, event_loop) = &mut cb.build().expect("Error creating event loop");
+    // let mut gameboy = rust_gameboy::gameboy::Gameboy::new(bytes, true);
+    // event::run(ctx, event_loop, &mut gameboy).expect("Error running event loop");
 }
 
 // fn get_context_builder() -> ggez::ContextBuilder {
@@ -22,7 +22,7 @@ fn main() {
 //     let window_mode: WindowMode = Default::default();
 //     let width = (SCREEN_WIDTH as usize) * PIXEL_SIZE;
 //     let height = (SCREEN_HEIGHT as usize) * PIXEL_SIZE;
-//     ContextBuilder::new("Rust Gameboy", "caklimas")
+//     ggez::ContextBuilder::new("Rust Gameboy", "caklimas")
 //         .window_setup(window_setup.title("Gameboy"))
 //         .window_mode(window_mode.dimensions(width as f32, height as f32))
 // }
