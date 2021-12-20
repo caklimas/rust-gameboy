4f6da95b6eee8cf582b9f244d4e5f005b6908bdd
Chris Klimas <christopherk@spotify.com>
2021-12-20 16:46:36 -0500

    Stub out wave pattern ram

--- src/apu/channel_3.rs
+++ src/apu/channel_3.rs
@@ -6,13 +6,14 @@ use crate::addresses::apu::{
 };
 
 use super::{
-    frequency_hi::FrequencyHi, sound_length_wave_pattern::SoundLengthWavePattern,
-    sound_on_off::SoundOnOff, volume_envelope::VolumeEnvelope,
+    frequency_hi::FrequencyHi, select_output_level::SelectOutputLevel, sound_on_off::SoundOnOff,
 };
 
 #[derive(Serialize, Deserialize, Default)]
 pub struct Channel3 {
     sound_on_off: SoundOnOff,
+    sound_length: u8,
+    select_output_level: SelectOutputLevel,
     frequency_lo: u8,
     frequency_hi: FrequencyHi,
 }
@@ -21,9 +22,9 @@ impl Channel3 {
     pub fn read(&self, address: u16) -> u8 {
         match address {
             CHANNEL_3_SOUND_ON_OFF => self.sound_on_off.0,
-            CHANNEL_3_SOUND_LENGTH => self.frequency_lo,
-            CHANNEL_3_SELECT_OUTPUT_LEVEL => todo!(),
-            CHANNEL_3_FREQUENCY_LO_DATA => self.frequency_lo,
+            CHANNEL_3_SOUND_LENGTH => panic!("Can't read address 0x{:4X}", address),
+            CHANNEL_3_SELECT_OUTPUT_LEVEL => self.select_output_level.0,
+            CHANNEL_3_FREQUENCY_LO_DATA => panic!("Can't read address 0x{:4X}", address),
             CHANNEL_3_FREQUENCY_HI_DATA => self.frequency_hi.0,
             _ => panic!("Invalid APU address 0x{:4X}", address),
         }
@@ -31,11 +32,11 @@ impl Channel3 {
 
     pub fn write(&mut self, address: u16, value: u8) {
         match address {
-            CHANNEL_3_SOUND_ON_OFF => todo!(),
-            CHANNEL_3_SOUND_LENGTH => todo!(),
-            CHANNEL_3_SELECT_OUTPUT_LEVEL => todo!(),
-            CHANNEL_3_FREQUENCY_LO_DATA => todo!(),
-            CHANNEL_3_FREQUENCY_HI_DATA => todo!(),
+            CHANNEL_3_SOUND_ON_OFF => self.sound_on_off.0 = value,
+            CHANNEL_3_SOUND_LENGTH => self.sound_length = value,
+            CHANNEL_3_SELECT_OUTPUT_LEVEL => self.select_output_level.0 = value,
+            CHANNEL_3_FREQUENCY_LO_DATA => self.frequency_lo = value,
+            CHANNEL_3_FREQUENCY_HI_DATA => self.frequency_hi.0 = value,
             _ => panic!("Invalid APU address 0x{:4X}", address),
         }
     }
--- src/apu/mod.rs
+++ src/apu/mod.rs
@@ -1,8 +1,11 @@
 use serde::{Deserialize, Serialize};
 
-use crate::addresses::apu::{CHANNEL_2_FREQUENCY_HI_DATA, CHANNEL_2_SOUND_LENGTH_WAVE_PATTERN};
+use crate::addresses::apu::{
+    CHANNEL_1_FREQUENCY_HI_DATA, CHANNEL_1_SWEEP_REGISTER, CHANNEL_2_FREQUENCY_HI_DATA,
+    CHANNEL_2_SOUND_LENGTH_WAVE_PATTERN, CHANNEL_3_FREQUENCY_HI_DATA, CHANNEL_3_SOUND_ON_OFF,
+};
 
-use self::channel_2::Channel2;
+use self::{channel_1::Channel1, channel_2::Channel2, channel_3::Channel3};
 
 pub mod channel_1;
 pub mod channel_2;
@@ -19,23 +22,34 @@ mod tests;
 
 #[derive(Serialize, Deserialize, Default)]
 pub struct Apu {
+    channel_1: Channel1,
     channel_2: Channel2,
+    channel_3: Channel3,
+    wave_pattern_ram: [u8; 16],
 }
 
 impl Apu {
     pub fn read(&self, address: u16) -> u8 {
         match address {
+            CHANNEL_1_SWEEP_REGISTER..=CHANNEL_1_FREQUENCY_HI_DATA => self.channel_1.read(address),
             CHANNEL_2_SOUND_LENGTH_WAVE_PATTERN..=CHANNEL_2_FREQUENCY_HI_DATA => {
                 self.channel_2.read(address)
             }
+            CHANNEL_3_SOUND_ON_OFF..=CHANNEL_3_FREQUENCY_HI_DATA => self.channel_3.read(address),
             _ => panic!("Invalid APU address 0x{:4X}", address),
         }
     }
 
     pub fn write(&mut self, address: u16, value: u8) {
         match address {
+            CHANNEL_1_SWEEP_REGISTER..=CHANNEL_1_FREQUENCY_HI_DATA => {
+                self.channel_1.write(address, value)
+            }
             CHANNEL_2_SOUND_LENGTH_WAVE_PATTERN..=CHANNEL_2_FREQUENCY_HI_DATA => {
-                self.channel_2.write(address, value);
+                self.channel_2.write(address, value)
+            }
+            CHANNEL_3_SOUND_ON_OFF..=CHANNEL_3_FREQUENCY_HI_DATA => {
+                self.channel_3.write(address, value)
             }
             _ => panic!("Invalid APU address 0x{:4X}", address),
         }
