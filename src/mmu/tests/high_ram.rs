1f596d1d22fb2f9018ac2b01f7c3e98ca1ae19aa
Christopher Klimas <caklimas@gmail.com>
2022-05-14 13:25:14 -0400

    Update high ram tests

--- src/mmu/tests/high_ram.rs
+++ src/mmu/tests/high_ram.rs
@@ -1,36 +1,25 @@
+use crate::addresses::high_ram::HIGH_RAM_LOWER;
+
 use super::super::high_ram::HighRam;
-use super::super::memory_sizes::HIGH_RAM;
 
 #[test]
 fn read_test() {
-    let address = 5;
+    let address = HIGH_RAM_LOWER + 5;
     let data = 5;
     let mut high_ram: HighRam = Default::default();
     high_ram.write(address, data);
 
-    let actual = high_ram.read(address);
-    assert_eq!(data, actual);
-
-    // Testing masking
-    let address = HIGH_RAM + address;
     let actual = high_ram.read(address);
     assert_eq!(data, actual);
 }
 
 #[test]
 fn write_test() {
-    let address = 5;
+    let address = HIGH_RAM_LOWER + 5;
     let data = 5;
     let mut high_ram: HighRam = Default::default();
     high_ram.write(address, data);
 
-    let actual = high_ram.read(address);
-    assert_eq!(data, actual);
-
-    // Testing masking
-    let data = data + 1;
-    let address = HIGH_RAM + address;
-    high_ram.write(address, data);
     let actual = high_ram.read(address);
     assert_eq!(data, actual);
 }
