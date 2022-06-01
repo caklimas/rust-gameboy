bef1e6cc9974a1d013513fecc15d229e5aa14959
Chris Klimas <christopherk@spotify.com>
2022-06-01 16:22:13 -0400

    Continue on working on mbc 3

---
+++ src/addresses/mbc/mbc3.rs
@@ -0,0 +1,8 @@
+pub const ROM_BANK_0_LOWER: u16 = 0x0000;
+pub const ROM_BANK_0_UPPER: u16 = 0x3FFF;
+
+pub const ROM_BANK_1_7F_LOWER: u16 = 0x4000;
+pub const ROM_BANK_1_7F_UPPER: u16 = 0x7FFF;
+
+pub const RAM_ENABLE_LOWER: u16 = 0x0000;
+pub const RAM_ENABLE_UPPER: u16 = 0x1FFF;
--- src/mbc/mbc3/mod.rs
+++ src/mbc/mbc3/mod.rs
@@ -1,28 +1,54 @@
-use crate::cartridge::cartridge_header::CartridgeHeader;
+use crate::{
+    addresses::mbc::mbc3::{ROM_BANK_0_LOWER, ROM_BANK_1_7F_LOWER},
+    cartridge::cartridge_header::CartridgeHeader,
+};
 
 use super::Mbc;
 
 pub struct Mbc3 {
+    ram: Vec<u8>,
+    ram_bank_number: u8,
     ram_enabled: bool,
     rom: Vec<u8>,
+    rom_bank_number: u8,
+    ram_enabled: bool,
 }
 
 impl Mbc3 {
     pub fn new(header: &CartridgeHeader, data: Vec<u8>) -> Self {
         Self {
+            ram: vec![0; header.ram_size.get_size()],
+            ram_bank_number: 0x00,
             ram_enabled: false,
             rom: data,
+            rom_bank_number: 0x01,
         }
+    }
+
+    fn write_ram_enabled(&mut self, data: u8) {
+        self.ram_enabled = data == ENABLE_RAM;
     }
 }
 
 impl Mbc for Mbc3 {
     fn read_ram(&self, address: u16) -> u8 {
+        if !self.ram_enabled {
+            return 0;
+        }
+
         todo!()
     }
 
     fn read_rom(&self, address: u16) -> u8 {
-        todo!()
+        match address {
+            ROM_BANK_0_LOWER..=ROM_BANK_0_LOWER => self.rom[address as usize],
+            ROM_BANK_1_7F_LOWER..=ROM_BANK_1_7F_LOWER => {
+                let offset = self.rom_bank_number as u16 * ROM_BANK_1_7F_LOWER;
+                let index = address - ROM_BANK_1_7F_LOWER + offset;
+                self.rom[index as usize]
+            }
+            _ => panic!("Invalid MBC3 address"),
+        }
     }
 
     fn write_ram(&mut self, address: u16, data: u8) {
