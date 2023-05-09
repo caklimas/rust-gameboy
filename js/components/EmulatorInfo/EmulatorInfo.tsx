0dc4f4829f16560215af04067f9043e0279dcdf5
Chris Klimas <christopherk@spotify.com>
2023-05-09 11:58:19 -0400

    Update the scale of the vram viewer

--- js/components/EmulatorInfo/EmulatorInfo.tsx
+++ js/components/EmulatorInfo/EmulatorInfo.tsx
@@ -6,54 +6,54 @@ import { Button, Modal } from "react-bootstrap";
 import chunk from "chunk";
 import styled from "styled-components";
 import { TileInfo } from "./TileInfo";
+import { CANVAS_WIDTH } from "./constants";
 
 type Props = {
-    show: boolean,
-    setShow: (show: boolean) => void;
-}
+  show: boolean;
+  setShow: (show: boolean) => void;
+};
+
+const GRID_GAP = CANVAS_WIDTH + 3;
 
 export function EmulatorInfo({ show, setShow }: Props) {
-    const emulator = useSelector<State, Emulator>(state => state.gameboy.emulator!);
-    const handleClose = useCallback(() => setShow(false), [setShow]);
-    if (!emulator) return null;
+  const emulator = useSelector<State, Emulator>(
+    (state) => state.gameboy.emulator!
+  );
+  const handleClose = useCallback(() => setShow(false), [setShow]);
+  if (!emulator) return null;
 
-    const cartridgeType = JSON.parse(emulator.get_header_info()).header.cartridge_type;
-    const colors = chunk(emulator.get_tiles(), 3);
-    const tiles = chunk(colors, 64);
-    console.log({ tileLEngth: tiles.length });
+  const cartridgeType = JSON.parse(emulator.get_header_info()).header
+    .cartridge_type;
+  const colors = chunk(emulator.get_tiles(), 3);
+  const tiles = chunk(colors, 64);
+  console.log({ tileLEngth: tiles.length });
 
-    const CanvasContainer = styled.div`
-        display: grid;
-        grid-template-rows: repeat(${Math.ceil(tiles.length / 16)}, 10px);
-        grid-template-columns: repeat(16, 10px);
-    `;
+  const CanvasContainer = styled.div`
+    display: grid;
+    grid-template-rows: repeat(${Math.ceil(tiles.length / 16)}, ${GRID_GAP}px);
+    grid-template-columns: repeat(16, ${GRID_GAP}px);
+  `;
 
-    return (
-        <>
-            <Modal show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title>
-                        Emulator Information
-                    </Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <p>
-                        Cartridge Type: {cartridgeType}
-                    </p>
-                    <CanvasContainer>
-                        {
-                            tiles.map(tile => (
-                                <TileInfo tile={tile} />
-                            ))
-                        }
-                    </CanvasContainer>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant='secondary' onClick={handleClose}>
-                        Close
-                    </Button>
-                </Modal.Footer>
-            </Modal>
-        </>
-    );
+  return (
+    <>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Emulator Information</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>Cartridge Type: {cartridgeType}</p>
+          <CanvasContainer>
+            {tiles.map((tile) => (
+              <TileInfo tile={tile} />
+            ))}
+          </CanvasContainer>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
 }
